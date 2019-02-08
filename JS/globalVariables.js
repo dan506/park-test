47b2d5c3f930ed2e00c9ed72ff05abaec3fd00ce
dan506 <1178975467@qq.com>
2019-02-08 20:02:01 +0800

    第一次提交

---
+++ JS/globalVariables.js
@@ -0,0 +1,6 @@
+﻿var originPath = window.location.origin;
+
+var IMAGE_PATH = originPath + "/Image";
+var VIDEO_PATH = originPath + "/Video";
+var LIB_ARCGIS_PATH = originPath + "/Lib/ArcGis";
+var LIB_ARCGIS_DOJO_PATH = LIB_ARCGIS_PATH + "/arcgis_js_api/library/4.10/dojo";
